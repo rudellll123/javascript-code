bcf5cce63d8b8e504aa45780541f48d7c4e84efc
RAHUL JHA <rj2544@srmist.edu.in>
2025-01-05 06:12:27 +0000

    DATATYPE

---
+++ 01.js/datatypes.js
@@ -0,0 +1,25 @@
+//let score = 100
+//console.log (typeof score);//numberr datatype
+
+
+let name = "rahul123"
+let valueInString = String(name)
+console.log(typeof valueInString)
+                                    // string datatypes
+console.log(valueInString)
+
+
+let score = "123abc"
+console.log(typeof score);  // STRING
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) //// points to be noted this is NaN VALUE 
+
+NOTES 
+
+//"33"=> 33
+// "33ABC" => NaN
+// TRUE=>1 , FALSE =>0,
+// " " = FALSE
+// "STRING" = TRUE 
+
