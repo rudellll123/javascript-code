a7514af17d26bd9a52f695a7558b7e5421302768
RAHUL JHA <rj2544@srmist.edu.in>
2025-01-05 04:20:13 +0000

    let const variables

---
+++ 01.js/02basics.js
@@ -0,0 +1,15 @@
+//const accountId = 123
+accountId = 456
+//console.log(accountId)   so we can't declear in constant variables
+
+
+
+
+const schoolId = 2311003012295
+let schoolName = "srm"
+let city = "chennai"
+let email = "rj2544@srmist.edu.in"
+place = "potheri"
+let placement;
+
+console.table([schoolId,schoolName,city,email,place,placement])
