6071cd01ad0b2b7dc23eafb1b0ed81e6230f5848
RAHUL JHA <rj2544@srmist.edu.in>
2025-01-06 03:38:28 +0000

    string

---
+++ 01.js/string.js
@@ -0,0 +1,22 @@
+const gameName = new String('rahuljha')
+console.log (gameName.length);// method .length
+
+console.log(gameName.toUpperCase()); //method  - .to upper case
+
+
+console.log(gameName.charAt(2));     // method - .charAt
+
+
+console.log (gameName.charCodeAt(2));
+
+
+console.log (gameName.toLowerCase());
+
+console.log (gameName.concat());
+
+console.log (gameName.slice(0,2));
+
+console.log (gameName.trim(0,2));
+
+console.log (gameName.trimEnd());
+
