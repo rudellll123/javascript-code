3b0d5df411a4bb6b5ff06fcb80ce1b363fbe88ba
RAHUL JHA <rj2544@srmist.edu.in>
2025-01-06 02:36:28 +0000

    conversion

---
+++ 01.js/conversion.js
@@ -0,0 +1,28 @@
+console.log("1"+2)
+
+console.log(1+"2")
+
+console.log("1"+2+3)
+
+//console.log(1+2+"3")      here 1 + 2 will add so value will be 33
+
+console.log("1"-2)
+
+console.log(1-"2")
+
+console.log(1-2-"3")
+
+console.log("1"-2-3)
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);// true
+
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined>=0);
+
+
+// type of value (Null)   - object 
