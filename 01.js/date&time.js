c12cc204c11bbce6755bd30898d26ff4dd48b3a7
RAHUL JHA <rj2544@srmist.edu.in>
2025-01-07 14:15:51 +0000

    date &time

---
+++ 01.js/date&time.js
@@ -0,0 +1,36 @@
+let myDate = new Date()
+
+// method of date
+console.log (myDate.toDateString());
+
+console.log (myDate.getDay());
+
+console.log (myDate.getFullYear());
+
+console.log (myDate.getMinutes());
+
+
+
+
+
+// time
+
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp); // value will come in milliseconds
+
+
+console.log(Math.floor(Date.now()/1000));// divide by 1000 value will come in second
+
+
+let newDate = new Date()
+console.log(newDate);
+
+
+// method 
+console.log(newDate.getDate());
+
+console.log(newDate.getDay());
+
+console.log(newDate.getHours());
