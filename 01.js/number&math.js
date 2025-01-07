d7ca096db80b1c624f3807c2adad51c21a4adcec
RAHUL JHA <rj2544@srmist.edu.in>
2025-01-07 13:58:18 +0000

    string

---
+++ 01.js/number&math.js
@@ -0,0 +1,42 @@
+//const score = newNumber(890)
+//console.log(score);
+
+
+//const otherNUmber = 23.896
+//console.log(otherNumber.toPrecison(3));
+
+let name = rahul 
+console.log(typeof name)
+
+
+//const score = newNumber(890)
+//console.log(score);
+
+
+//const otherNUmber = 23.896
+//
+// 
+// console.log(otherNumber.toPrecison(3));
+
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+// different methods of number & methods
+
+const math = 1000
+console.log(math);
+
+
+console.log(math.abs(-3));
+
+console.log(Math.round(4.3));
+
+console.log(Math.ceil(6.8));
+
+console.log(Math.floor(5.9));
+
+
+console.log(math);
